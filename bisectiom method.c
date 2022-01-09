27d8ecbc78e16c98d742851ed1bd0d9be99b36d0
sauravkhanal <sauravkhanal635@gmail.com>
2022-01-10 00:02:41 +0545

    First Commit )

---
+++ bisectiom method.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define e.001
+#define f(x) x*x*x-4*x-9
+void main()
+{
+
+    float x1,x2,x0,a,b,c,E;
+    int n=0;
+    do{
+        printf("Enter values for x1 and x2 :    ");
+        scanf("%f %f",&x1,&x2);
+        a=f(x1);
+        b=f(x2);
+    }while(a*b>0);
+
+    do{
+        x0=(x1+x2)/2;
+        n++;
+        c=f(x0);
+        if(a*c<0)
+        {
+            x2=x0;
+        }
+        else
+            x1=x0;
+
+        E=fabs(x2-x1);
+    }while(E>e);
+
+    printf("\n the root is %f",x0);
+
+}
